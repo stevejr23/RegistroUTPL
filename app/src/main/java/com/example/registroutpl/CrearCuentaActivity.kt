e540420e9d7c16efdcfbcd5d0b56c7ea1d859516
tivis <stevej4r412@hotmail.com>
2022-12-16 18:44:02 -0500

    UTPL

---
+++ app/src/main/java/com/example/registroutpl/CrearCuentaActivity.kt
@@ -0,0 +1,11 @@
+package com.example.registroutpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearCuentaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_cuenta)
+    }
+}
